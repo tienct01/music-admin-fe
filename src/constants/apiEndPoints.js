2a24e4187e829138a810e8f169d036d021cf5cc7
tiendvd <doanvanduytien@gmail.com>
2023-12-18 18:16:01 +0700

    fix base url

--- src/constants/apiEndPoints.js
+++ src/constants/apiEndPoints.js
@@ -1,4 +1,4 @@
-export const BASE_URL = import.meta.env.BASE_URL;
+export const BASE_URL = process.env.BASE_URL;
 export const endPoint = {
 	login: `${BASE_URL}/login`,
 	myProfile: `${BASE_URL}/my_profile`,
